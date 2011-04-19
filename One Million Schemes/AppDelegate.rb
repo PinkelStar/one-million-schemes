aeafc56d3caec1ffa9289add87c4d69f3a731f22
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-04-19 10:39:51 +0200

    Initial Commit

---
+++ One Million Schemes/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  One Million Schemes
+#
+#  Created by Josh Kalderimis on 4/19/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
