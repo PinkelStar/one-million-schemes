15c90d942d4e80430527ea4f0ab30170c4a7254d
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-04-27 17:30:35 +0200

    commit commit commit

--- One Million Schemes/AppDelegate.rb
+++ One Million Schemes/AppDelegate.rb
@@ -3,13 +3,107 @@
 #  One Million Schemes
 #
 #  Created by Josh Kalderimis on 4/19/11.
-#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#  Copyright 2011 Zwapp. All rights reserved.
 #
 
 class AppDelegate
-  attr_accessor :window
-  def applicationDidFinishLaunching(a_notification)
-    # Insert code here to initialize your application
+  attr_accessor :window, :twitterPromptWindow
+  attr_accessor :progressIndicator, :startButton, :statusLabel
+  attr_accessor :twitterName, :errorLabel
+  attr_accessor :notificationCenter
+  
+  def applicationDidFinishLaunching(notification)
+    setupNotificationsObservers
+  end
+  
+  def setupNotificationsObservers
+    @notificationCenter = NSNotificationCenter.defaultCenter
+    
+    @notificationCenter.addObserver(self,
+                                    selector: "appsFound:",
+                                    name: "AppsFound",
+                                    object: nil)
+    
+    @notificationCenter.addObserver(self,
+                                    selector: "processingFinished:",
+                                    name: "ProcessingFinished",
+                                    object: nil) 
+    
+    @notificationCenter.addObserver(self,
+                                    selector: "uploadingFinished:",
+                                    name: "UploadingFinished",
+                                    object: nil) 
+  end
+  
+  def startUpload(sender)
+    toggleUIState
+    IpaProcessor.updateApps do |appList|
+      @recentAppList = appList
+      promptForTwitterName
+    end
+  end
+  
+  def promptForTwitterName
+    NSApp.beginSheet(twitterPromptWindow, 
+                     modalForWindow: window,
+                     modalDelegate: self,
+                     didEndSelector: "didEndSheet:returnCode:contextInfo:",
+                     contextInfo: nil)
+  end
+  
+  def uploadWithoutTwitterName(sender)
+    NSApp.endSheet(twitterPromptWindow)
+    self.twitterName.stringValue = nil
+    uploadAppList
+  end
+  
+  def uploadWithTwitterName(sender)
+    NSApp.endSheet(twitterPromptWindow)
+    TwitterNameChecker.start(self.twitterName.stringValue) do |valid|
+      if valid
+        uploadAppList
+      else
+        self.errorLabel.stringValue = "can't seem to find it"
+        promptForTwitterName
+      end
+    end
+  end
+  
+  def didEndSheet(sheet, returnCode: returnCode, contextInfo: contextInfo)
+    sheet.orderOut(self)
+  end
+  
+  def uploadAppList
+    PlistUploader.start(@recentAppList, self.twitterName.stringValue) do
+      @notificationCenter.postNotificationName("UploadingFinished", object: self)
+      toggleUIState
+    end
+  end
+  
+  def toggleUIState
+    if startButton.isEnabled
+      progressIndicator.startAnimation(self)
+      startButton.enabled = false
+    else
+      progressIndicator.stopAnimation(self)
+      startButton.enabled = true
+    end
+  end
+  
+  def appsFound(notification)
+    self.statusLabel.stringValue = "#{notification.userInfo[:appsCount]} Apps Found"
+  end
+  
+  def processingFinished(notification)
+    self.statusLabel.stringValue = "Time to upload ..."
+  end
+  
+  def uploadingFinished(notification)
+    self.statusLabel.stringValue = "All Done!"
+  end
+  
+  def windowWillClose(sender)
+    NSApp.terminate(self)
   end
 end
 
---
+++ One Million Schemes/PlistUploader.rb
@@ -0,0 +1,67 @@
+#
+#  PlistUploader.rb
+#  One Million Schemes
+#
+#  Created by Josh Kalderimis on 4/19/11.
+#  Copyright 2011 Zwapp. All rights reserved.
+#
+require 'json'
+
+class PlistUploader
+  attr_reader :response, :responseBody
+  
+  def self.start(plistData, twitterName, &block)
+    uploader = self.new(&block)
+    uploader.start(plistData, twitterName)
+  end
+
+  def initialize(&block)
+    url = NSURL.URLWithString("http://schemes.zwapp.com/schemes")
+    @request = NSMutableURLRequest.requestWithURL(url)
+    
+    @request.setHTTPMethod("POST")
+    
+    @request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    @request.addValue("application/json", forHTTPHeaderField: "Accepts")
+    
+    @callback = block
+  end
+  
+  def start(plistData, twitterName)
+    hash = { :plists => plistData }
+    hash[:twitter_name] = twitterName if twitterName && !twitterName.empty?
+    
+    json = hash.to_json
+
+    @request.setHTTPBody(json.dataUsingEncoding(NSUTF8StringEncoding))
+    
+    NSURLConnection.connectionWithRequest(@request, delegate:self)
+  end
+  
+  def connection(connection, didReceiveResponse:response)
+    @response = response
+    @downloadData = NSMutableData.data
+  end
+  
+  def connection(connection, didReceiveData:data)
+    @downloadData.appendData(data)
+  end
+
+  def connection(connection, didFailWithError: error)
+    NSLog("oh no! we gotz an error!")
+    NSLog(error.userInfo.inspect)
+  end
+  
+  def connectionDidFinishLoading(connection)
+    case @response.statusCode
+    when 200...300
+      NSLog("#{@response.statusCode} - All good in the hood")
+      @responseBody = NSString.alloc.initWithData(@downloadData, encoding:NSUTF8StringEncoding)
+      @callback.call if @callback.respond_to?(:call)
+    when 300...400
+      NSLog("#{@response.statusCode} - Got a redirect :(") # need to handle it better
+    else
+      NSLog("#{@response.statusCode} - Uploading the Plist data failed :(")
+    end
+  end
+end
